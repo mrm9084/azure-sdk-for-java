16834db56d3dffe021afe1071a1ece2c500f9b99
Matt Metcalf <mrm9084@gmail.com>
2023-03-24 13:13:08 -0700

    Create module-info.java

---
+++ sdk/spring/spring-cloud-azure-feature-management/src/main/java/module-info.java
@@ -0,0 +1,35 @@
+module com.azure.spring.cloud.feature.management {
+    exports com.azure.spring.cloud.feature.management.targeting;
+
+    exports com.azure.spring.cloud.feature.management.models;
+
+    exports com.azure.spring.cloud.feature.management;
+
+    exports com.azure.spring.cloud.feature.management.implementation;
+
+    exports com.azure.spring.cloud.feature.management.filters;
+    
+    exports com.azure.spring.cloud.feature.management.implementation.targeting to com.fasterxml.jackson.databind;
+
+    requires com.fasterxml.jackson.annotation;
+
+    requires com.fasterxml.jackson.core;
+
+    requires com.fasterxml.jackson.databind;
+
+    requires org.reactivestreams;
+
+    requires org.slf4j;
+
+    requires reactor.core;
+
+    requires spring.beans;
+
+    requires spring.boot;
+
+    requires spring.context;
+
+    requires spring.core;
+    
+    opens com.azure.spring.cloud.feature.management to spring.core;
+}
